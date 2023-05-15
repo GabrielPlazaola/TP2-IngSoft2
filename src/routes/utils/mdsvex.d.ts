9ded7b392833b33a062b2c72b960c85a619f5fd1
Gabriel <70722542+GabrielPlazaola@users.noreply.github.com>
2023-05-15 19:57:50 -0300

    .

--- src/routes/utils/mdsvex.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.md' {
-  import type { SvelteComponentDev } from 'svelte/internal';
-
-  export default class Comp extends SvelteComponentDev {
-    $$prop_def: {};
-  }
-  export const metadata: Record<string, any>;
-}
